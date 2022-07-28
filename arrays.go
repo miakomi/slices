a76f88813f3bd25789aa4426470f83f561aecf91
miakomi <leosha69.com@gmail.com>
2022-07-28 14:48:46 +0300

    First commit. Add first sorted function Sort(a []int)

--- arrays.go
+++ arrays.go
@@ -1,1 +1,18 @@
+package arrays
+
+func Sort(a []int) []int {
+  left := 0
+  right := len(a)-1
+  temp := 0
+  
+  for left < right {
+    temp = a[left]
+    a[left] = a[right]
+    a[right] = temp
+
+    left ++
+    right --
+  }
+  return a
+}
 
